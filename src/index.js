0e63331279cbcb1eac3bb0625c90bb50c0b27312
Krasimir Tsonev <krasimir@outset.ws>
2016-01-03 21:13:46 +0200

    Boilerplate

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import 'better-log/install';
+
+module.exports = function ({ types: t }) {
+  return {
+    visitor: {
+      Program(path, file) {
+        path.unshiftContainer('body', t.expressionStatement(t.stringLiteral('use helloworld')));
+      }
+    }
+  };
+};
